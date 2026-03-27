fda539788e11b5af5d8658a48c9658c460458ed3
PRADEEP MK <pradeepmk799@gmail.com>
2026-03-27 11:47:33 +0530

    cfg

--- pro1/terraform/main.tf
+++ pro1/terraform/main.tf
@@ -1,3 +1,4 @@
+# provider
 terraform {
   required_providers {
     aws = {
@@ -10,31 +11,38 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-#VPC
+
+# vpc
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "~> 5.0"
+
   name = "multi-project-vpc"
   cidr = "10.0.0.0/16"
+
   azs             = ["us-east-1a", "us-east-1b"]
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-  enable_dns_hostnames   = true
-  enable_dns_support     = true
+
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  map_public_ip_on_launch = true
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
   }
 }
 
-#SECURITY GROUP
+# security-group
 resource "aws_security_group" "all_traffic_sg" {
   name_prefix = "allow-all-traffic-"
   vpc_id      = module.vpc.vpc_id
+
   ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
@@ -43,27 +51,38 @@ resource "aws_security_group" "all_traffic_sg" {
   }
 }
 
-#EKS 
+# eks
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 19.0"
-  cluster_name    = "multi-project-cluster"
+
+  cluster_name    = "multi-project-cluster-2"
   cluster_version = "1.28"
+
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
+
   cluster_endpoint_public_access = true
+
   cluster_addons = {
     coredns    = {}
     kube-proxy = {}
     vpc-cni    = {}
   }
+
+  # node-group
   eks_managed_node_groups = {
     workers = {
-      min_size     = 3
-      max_size     = 3
-      desired_size = 3
-      instance_types = ["t3.medium"]
-      additional_security_group_ids = [aws_security_group.all_traffic_sg.id]
+      min_size     = 1
+      max_size     = 2
+      desired_size = 2
+
+      instance_types = ["t3.micro"]
+      ami_type       = "AL2_x86_64"
+
+      additional_security_group_ids = [
+        aws_security_group.all_traffic_sg.id
+      ]
     }
   }
 }
